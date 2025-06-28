a53e2d627ce924fa962f31a21a42d86bdbccc108
Tareq-Alhamaly <tarek.alhamaly@nawa-eng.com.ly>
2025-06-28 22:04:48 +0100

    app.py

--- app.py
+++ app.py
@@ -4,40 +4,6 @@ import numpy as np
 import os
 import gdown
 
-# ---------- Authentication ----------
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["authenticated"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["authenticated"] = False
-
-    if "authenticated" not in st.session_state:
-        st.text_input("Enter Access Password:", type="password", on_change=password_entered, key="password")
-        st.stop()
-    elif not st.session_state["authenticated"]:
-        st.error("❌ Incorrect password.")
-        st.stop()
-
-check_password()
-
-# ---------- Free Trial & Subscription ----------
-FREE_TRIAL_LIMIT = 10
-if "prediction_count" not in st.session_state:
-    st.session_state["prediction_count"] = 0
-if "subscribed" not in st.session_state:
-    st.session_state["subscribed"] = False  # Change to True if user is subscribed
-
-def can_predict():
-    if st.session_state["subscribed"]:
-        return True
-    return st.session_state["prediction_count"] < FREE_TRIAL_LIMIT
-
-def record_prediction():
-    if not st.session_state["subscribed"]:
-        st.session_state["prediction_count"] += 1
-
 
 @st.cache_resource
 def load_model():
